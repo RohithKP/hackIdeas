c660966371934d1cb9b34556ef298d16e97ecd05
rki-financial-com <rki@financial.com>
2021-10-03 19:52:31 +0530

    service test case added

--- src/app/services/idea.service.spec.ts
+++ src/app/services/idea.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { IdeaService } from './idea.service';
 
 describe('IdeaService', () => {
+  let httpTestingController: HttpTestingController;
   let service: IdeaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
     service = TestBed.inject(IdeaService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getData should use GET to retrieve data', () => {
+    service.getIdeas().subscribe();
+
+    const testRequest = httpTestingController.expectOne('http://localhost:3000/ideas');
+
+    expect(testRequest.request.method).toEqual('GET');
   });
 });
