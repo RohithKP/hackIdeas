132a5f1203026c5dbb127433d21ada87125e398e
rki-financial-com <rki@financial.com>
2021-10-03 11:01:45 +0530

    Basic layout and routing added for Home component and not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { SharedModule } from './shared/shared.module';
+
+
+const routes: Routes = [
+  {   path: '',   component: HomeComponent   },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
